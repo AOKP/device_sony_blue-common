2689c63d6a586886d333973a5742a4c9e363a1a8
Lalit Maganti <lalitmaganti@gmail.com>
2014-03-26 18:12:26 +0000

    blue: add static tad to sbin
    Change-Id: I3210b6d1836444980e2ebd6b975c83ad2cfaf7f1

--- blue.mk
+++ blue.mk
@@ -54,6 +54,11 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/etc/init.sony.bt.sh:system/etc/init.sony.bt.sh \
     $(COMMON_PATH)/rootdir/ueventd.qcom.rc:root/ueventd.qcom.rc
 
+# Additional sbin stuff
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/sbin/wait4tad_static:root/sbin/wait4tad_static \
+    $(LOCAL_PATH)/rootdir/sbin/tad_static:root/sbin/tad_static
+
 # Key and touchscreen files
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/usr/idc/sensor00_f11_sensor0.idc:system/usr/idc/sensor00_f11_sensor0.idc \
