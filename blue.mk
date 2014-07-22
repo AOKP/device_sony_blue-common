f5e3d3245114e4efc818871fb5d84221560bdaf2
Brinly Taylor <uberlaggydarwin@gmail.com>
2014-07-22 19:05:52 +0930

    blue: alphabetize the packages included
    Change-Id: I6ea6a2d64e6e609327c4a9c9cb206b59e82f9c16

--- blue.mk
+++ blue.mk
@@ -105,7 +105,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/sap.conf:system/etc/sap.conf
 
-
 # SEC Config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/system/etc/sec_config:system/etc/sec_config
@@ -154,7 +153,7 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     tinymix
 
-# BT
+# Bluetooth
 PRODUCT_PACKAGES += \
     hci_qcomm_init
 
@@ -162,21 +161,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.sony \
     camera.msm8960 \
-    libmmcamera_interface2 \
-    libmmcamera_interface
+    libmmcamera_interface \
+    libmmcamera_interface2
 
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.msm8960
 
-# Wifi service
-PRODUCT_PACKAGES += \
-    wcnss_service
-
-# WLAN
+# Wifi services
 PRODUCT_PACKAGES += \
     libwfcu \
-    mac-update
+    mac-update \
+    wcnss_service
 
 # Misc
 PRODUCT_PACKAGES += \
